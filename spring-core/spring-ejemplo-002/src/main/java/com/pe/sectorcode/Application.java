7bca99cfa24efc4e74f5fde7f34c7a7e43e8ca2c
sectorcode <josue.lopez.acosta@sectorcode.com>
2021-07-04 23:57:13 -0500

    Segundo ejemplo

--- spring-core/spring-ejemplo-002/src/main/java/com/pe/sectorcode/Application.java
+++ spring-core/spring-ejemplo-002/src/main/java/com/pe/sectorcode/Application.java
@@ -4,17 +4,16 @@ import com.pe.sectorcode.beans.HolaMundo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application
 {
-
     public static void main(String[] args) {
         
-        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
         HolaMundo          hm         = (HolaMundo)appContext.getBean("holaMundoBean");
         System.out.println(hm.getSaludo());
         
         ((ConfigurableApplicationContext)appContext).close();
     }
-    
 }
